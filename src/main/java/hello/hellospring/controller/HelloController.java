1f6f33c79749928ccfb51633d0942ea73b0ae5c1
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-07-07 13:10:57 +0900

    2022-07-07 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 내용 [1]

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,51 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //data 자체를 그대로 내보내줌 (소스 보기하면 코드 없고 데이터만 전달)
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody //JSON 방식의 data 전달 방식
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
+}
---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,57 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    /**
+     * 같은 저장소를 사용하고 있는 것이 아니기 때문에 정확한 테스트 방법이 아님.
+     * 필드 주입이 아닌 생성자를 활용한 주입이 필요함.
+     * 외부에서 memberRepository를 집어넣어줌. Dependency Injection(DI) 의존성 주입
+     */
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     *회원가입
+     */
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원X
+        validateDuplicateMember(member); //중복 회원 검증 (ctrl + alt + m으로 코드 리팩토링) => method화.
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * service의 경우 repository에 비해 method의 명칭이 비즈니스에 가깝다.
+     * 그래야지 기획자와 개발자간의 소통이 원할하게 이뤄질 수 있다.
+     */
+
+    /**
+     *  전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
